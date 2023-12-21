ae8020f0f28be6b6b3533578d8b454873c911497
farrukh.khan <farrukh.khan@sybrid.com>
2023-12-21 21:24:16 +0500

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+
 
 const app = express();
 dotenv.config();
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("API is Running Successfully");
 });
@@ -18,4 +21,4 @@ app.get("/api/chat/:id", (req, res) => {
   res.send(singleChat);
 });
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server started on PORT ${port}`));
+app.listen(port, console.log(`Server started on PORT ${port}`.yellow.bold));
